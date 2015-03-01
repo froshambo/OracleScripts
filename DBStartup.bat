e0384ef0906129a89e999595bb630b4610076b76
froshambo <tribpickle@gmail.com>
2015-03-01 09:28:18 -0700

    initial commit of database startup script

---
+++ DBStartup.bat
@@ -0,0 +1,6 @@
+::Database and Listener startup script
+
+E:\Tools\cx_OracleDBATools\StartDB.exe --config-file="E:\Tools\cx_OracleDBATools\OracleControl-win32.ini" marshal
+E:\Tools\cx_OracleDBATools\StartDB.exe --config-file="E:\Tools\cx_OracleDBATools\OracleControl-win32.ini" sandbox
+LSNRCTL
+start Listener
