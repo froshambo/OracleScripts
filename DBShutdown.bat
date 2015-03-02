25491c824443a26c4a09fbfe6897d5c644f7cd93
froshambo <tribpickle@gmail.com>
2015-03-01 17:48:34 -0700

    initial commit of shutdown script

---
+++ DBShutdown.bat
@@ -0,0 +1,5 @@
+::Database and Listener shutdown script
+
+LSNRCTL stop listener
+E:\Tools\cx_OracleDBATools\StopDB.exe --config-file="E:\Tools\cx_OracleDBATools\OracleControl-win32.ini" sandbox
+E:\Tools\cx_OracleDBATools\StopDB.exe --config-file="E:\Tools\cx_OracleDBATools\OracleControl-win32.ini" marshal
